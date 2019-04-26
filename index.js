7db45380ed831a15834e276de5da08b2f4cc4661
Anais Trope <trope.anais@gmail.com>
2019-04-26 16:16:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ if (name === "Susan") {
   console.log("The name is correct.")
 }
 else {
-console.log("Expected the name the name to be Susan. ")
+console.log("Expected the name to be Susan. ")
 }
